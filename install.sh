1a1edaa7b9781ebb11f8de7decf26f5003a15d1b
Devang Patel <dpatel@hach.com>
2025-11-05 13:17:35 -0700

    BUGFIX: Protobuf build was using extra CMAKE install PATH that was causing 2 separate protobuf protoc under /usr/bin & /usr/local/bin. Now with default install location set by Google i.e. /usr/local/bin being used so that each it will be overritten by Google protobuf CMAKE install and all will work well for NPD.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ pushd /tmp
 git clone --progress --verbose --recurse-submodules --jobs=$(nproc) --branch=25.x https://github.com/protocolbuffers/protobuf.git
 pushd protobuf/
 git checkout 4a2aef570deb2bfb8927426558701e8bfc26f2a4 
-cmake . -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_LIBPROTOC=ON -DABSL_BUILD_TESTING=OFF -Dprotobuf_BUILD_SHARED_LIBS=ON -DABSL_PROPAGATE_CXX_STD=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
+cmake . -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_LIBPROTOC=ON -DABSL_BUILD_TESTING=OFF -Dprotobuf_BUILD_SHARED_LIBS=ON -DABSL_PROPAGATE_CXX_STD=ON ..
 make -j$(nproc)
 make install
 ldconfig
@@ -15,6 +15,14 @@ ldconfig
 popd
 rm -rf protobuf
 popd
+
+# Get protobuf version and informations.
+echo -e "\n---------------------------------------------"
+echo -e "\t🛠️ Google Protobuf Info"
+echo -e "---------------------------------------------"
+echo -e "Symlink:\t$(which protoc)\nTarget :\t$(readlink -f $(which protoc))"
+echo -e "Version:\t$($(which protoc) --version)"
+echo -e "\n"
 
 # Build Protobufjson
 make clean
@@ -26,6 +34,10 @@ cp ProtoToJson /usr/bin/
 cp JsonToProto /usr/bin/ 
 ln -s -r /usr/bin/JsonToProto /usr/bin/JsonToHeader
 
-
-
+echo -e "\n-----------------------------"
+echo -e "\t📦 Version Info"
+echo -e "-----------------------------"
+echo -e "ProtoToJson:\t$($(which ProtoToJson) --help 2>&1 | grep '^Version:' | cut -d':' -f2 | xargs)"
+echo -e "JsonToProto:\t$($(which JsonToProto) --help 2>&1 | grep '^Version:' | cut -d':' -f2 | xargs)"
+echo -e "\n"
 
